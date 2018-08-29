db9897985fc2572c74af8629a3772c001969a565
charm <404373224@qq.com>
2018-08-29 14:22:13 +0800

    erc721标准参考代码

--- contracts_develop/lt_erc721/lt_erc721.cpp
+++ contracts_develop/lt_erc721/lt_erc721.cpp
@@ -1,3 +1,198 @@
 #include "lt_erc721.hpp"
 
 
+void lt_erc721::transfer_balances(account_name from, account_name to, int64_t amount)
+{
+    if(from != 0){
+        auto fromitr = _accounts.find(from);
+        _accounts.modify(fromitr, 0, [&](auto &a){
+            a.balance -= amount;
+        });
+    }
+
+    if(to != 0){
+        auto toitr = _accounts.find(to);
+
+        if(toitr != _accounts.end()){
+            _accounts.modify(toitr, 0, [&](auto &a){
+                a.balance += amount;
+            });
+        }else{
+            _accounts.emplace(from, [&](auto &a) {
+                a.owner = to;
+                a.balance = amount;
+            });
+        }
+
+        
+    }
+}
+
+
+bool lt_erc721::_owns(account_name claimant, uint64_t token_id)
+{
+    return owner_of(token_id) == claimant;
+}
+
+
+uint64_t lt_erc721::total_supply()
+{
+    auto tokitr = _tokens.begin();
+    uint64_t token_id = 0;
+    while(tokitr != _tokens.end()){
+        token_id++;
+        tokitr++;
+    }
+
+    return token_id;
+}
+
+uint64_t lt_erc721::balance_of(account_name owner) 
+{
+    auto account = _accounts.find(owner);
+    return account->balance;
+}
+
+account_name lt_erc721::owner_of(uint64_t token_id)
+{
+    auto token = _tokens.find(token_id);
+    return token->owner;
+}
+
+void lt_erc721::approve(account_name from, account_name to, uint64_t token_id)
+{            
+    require_auth(from);
+
+    auto tokitr = _tokens.find(token_id);
+
+    //check to see if approver owns the token
+    if(tokitr == _tokens.end() || tokitr->owner != from ){            
+        eosio_assert(false, "token does not exist");
+    }
+
+    auto allowanceitr = _allowances.find(token_id);
+    if (allowanceitr == _allowances.end())
+    {            
+        _allowances.emplace(token_id, [&](auto &a) {                
+            a.to = to;
+            a.token_id = token_id;                 
+        });
+    }
+    else
+    {
+        _allowances.modify(allowanceitr, 0, [&](auto &a) {
+            a.to = to;                
+        });
+    }
+}
+
+void lt_erc721::mint(account_name owner, vector<string> keys , vector<string> values , bool is_frozen)
+{
+    require_auth(_self);
+
+    print("minting token to", name{owner});
+
+    uint64_t token_id = total_supply() + 1;
+
+    print("new token id", token_id);
+
+    auto accountitr = _accounts.find(owner);
+
+    if(accountitr == _accounts.end()){
+        _accounts.emplace(_self, [&](auto &a){
+            a.owner = owner;
+            a.balance = 1;
+        });
+    }
+
+    vector<var> vars;
+    for(int i = 0; i<keys.size(); i++){
+        vars.push_back(var {keys[i], values[i]} );
+    }
+
+    _tokens.emplace(_self, [&](auto &a) {
+        a.owner = owner;
+        a.issuer = _self;
+        a.id = token_id;  
+        a.frozen = is_frozen; 
+        a.keys = keys;
+        a.values = values;   
+        a.vars = vars;                              
+    });
+}
+
+void lt_erc721::transfer(account_name sender, account_name to, uint64_t token_id)
+{
+    require_auth(sender);
+
+    //find token
+    auto tokenitr = _tokens.find(token_id);
+
+    //make sure token exists and sender owns it
+    if(tokenitr != _tokens.end() && tokenitr->owner == sender){
+        //update token's owner
+        _tokens.modify(tokenitr, 0, [&](auto &a){
+            a.owner = to;
+        });
+
+        //increment/decrement balances 
+        transfer_balances(sender, to);  
+    } 
+}
+
+
+void lt_erc721::transferfrom(account_name sender, account_name from, account_name to, uint64_t token_id)
+{        
+    require_auth(sender);
+
+    //try to find allowance and token
+    auto allowanceitr = _allowances.find(token_id);
+    auto tokenitr = _tokens.find(token_id);
+
+    //make sure the token/allowances for token exist and the users match
+    if (tokenitr != _tokens.end() && tokenitr->owner == from &&
+        allowanceitr != _allowances.end() && allowanceitr->to == sender)
+    {
+        _tokens.modify(tokenitr, 0, [&](auto &a){
+            a.owner = to;
+        });     
+        transfer_balances(from, to);     
+        _allowances.erase(allowanceitr);
+    } 
+}     
+
+void lt_erc721::burn(account_name burner, uint64_t token_id)
+{
+    require_auth(burner);
+    auto tokenitr = _tokens.find(token_id);
+    
+    if(tokenitr != _tokens.end() && tokenitr->owner == burner){
+        transfer_balances(burner, 0);     
+        //_tokens.erase(tokenitr);
+        _tokens.modify(tokenitr, 0, [&](auto &a){
+            a.owner = 0;
+        }); 
+    }
+}
+
+void lt_erc721::burnfrom(account_name burner, account_name from, uint64_t token_id)
+{
+    require_auth(burner);
+
+    //try to find allowance and token
+    auto allowanceitr = _allowances.find(token_id);
+    auto tokenitr = _tokens.find(token_id);
+
+    //make sure the token/allowances for token exist and the users match
+    if (tokenitr != _tokens.end() && tokenitr->owner == from &&
+        allowanceitr != _allowances.end() && allowanceitr->to == burner)
+    {
+        //_tokens.erase(tokenitr);    
+        transfer_balances(from, 0);     
+        //_allowances.erase(allowanceitr);
+        _tokens.modify(tokenitr, 0, [&](auto &a){
+            a.owner = 0;
+        }); 
+    }
+}
+
--- contracts_develop/lt_erc721/lt_erc721.hpp
+++ contracts_develop/lt_erc721/lt_erc721.hpp
@@ -3,17 +3,103 @@
  *  @copyright defined in eos/LICENSE.txt
  */
 #include <eosiolib/eosio.hpp>
+#include <string>
 
 using namespace eosio;
+using namespace std;
 
-class lt_erc721 : public eosio::contract {
-  public:
-      using contract::contract;
+class lt_erc721 : public eosio::contract
+{
+    public:
+    lt_erc721(account_name self)
+        : contract(self), _accounts(_self, _self), _allowances(_self, _self), _tokens(_self, _self) {}
 
-      /// @abi action 
-      void hi( account_name user ) {
-         print( "Hello, ", name{user} );
-      }
+
+    private:
+        struct var 
+        {            
+            std::string key;       
+            std::string value;        
+        };
+
+        //@abi table tokens i64 
+        struct token 
+        {
+            uint64_t id;
+            
+            bool frozen;
+
+            //user associations
+            account_name owner;
+            account_name issuer;        
+
+            //data        
+            std::vector<std::string> keys;
+            std::vector<std::string> values;
+
+            std::vector<var> vars;
+
+            uint64_t primary_key() const { return id; }
+            
+            EOSLIB_SERIALIZE( token, (id)(frozen)(owner)(issuer)(keys)(values)(vars) )
+        };
+
+        eosio::multi_index<N(tokens), token> _tokens;
+
+        //@abi table accounts i64 
+        struct account
+        {
+            account_name owner;            
+            uint64_t balance;
+
+            uint64_t primary_key() const { return owner; }
+
+            EOSLIB_SERIALIZE( account, (owner)(balance))
+        };
+
+        eosio::multi_index<N(accounts), account> _accounts;
+
+        //@abi table allowances i64 
+        struct allowance
+        {
+            uint64_t token_id;   
+            account_name to;                         
+
+            uint64_t primary_key() const { return token_id; }
+
+            EOSLIB_SERIALIZE( allowance, (token_id)(to))
+        };
+
+        eosio::multi_index<N(allowances), allowance> _allowances;
+
+        void transfer_balances(account_name from, account_name to, int64_t amount=1);
+
+        bool _owns(account_name claimant, uint64_t token_id);
+
+    public:
+        // Required methods
+        uint64_t total_supply();
+
+        //returns balance of
+        uint64_t balance_of(account_name owner);
+
+        //returns who owns a token
+        account_name owner_of(uint64_t token_id);
+
+        void approve(account_name from, account_name to, uint64_t token_id);
+
+        void mint(account_name owner, vector<string> keys = {}, vector<string> values = {}, bool is_frozen=false);
+
+        void transfer(account_name sender, account_name to, uint64_t token_id);
+
+
+        void transferfrom(account_name sender, account_name from, account_name to, uint64_t token_id);
+
+        void burn(account_name burner, uint64_t token_id);
+
+        void burnfrom(account_name burner, account_name from, uint64_t token_id);
 };
 
-EOSIO_ABI( lt_erc721, (hi) )
+//typedef erc721<instrument_data> instrument;
+
+EOSIO_ABI(lt_erc721, (transfer)(mint)(approve)(transferfrom)(burn)(burnfrom) )
